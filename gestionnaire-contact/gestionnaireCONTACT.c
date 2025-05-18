d0fe93c41e6c4cacfddd058f6c0734c57c33502e
arhhhhh404 <mrtn.kerloch@email.com>
2025-05-19 00:59:21 +0200

    commit2

--- gestionnaire-contact/gestionnaireCONTACT.c
+++ gestionnaire-contact/gestionnaireCONTACT.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 typedef struct {
     char nom[50];
@@ -7,29 +9,65 @@ typedef struct {
     char email[100];
 } Contact;
 
+void ajouter_contact(Contact **contacts, int *taille) {
+    *contacts = realloc(*contacts, (*taille + 1)*sizeof(Contact));
+
+    if (*contacts == NULL) {
+        printf("Erreur d'allocation mémoire\n");
+        return;
+    }
+
+    printf("Nom: ");
+    scanf("%s", (*contacts)[*taille].nom);
+    printf("Prénom: ");
+    scanf("%s", (*contacts)[*taille].prenom);
+    printf("Téléphon: ");
+    scanf("%s", (*contacts)[*taille].telephone);
+    printf("Email: ");
+    scanf("%s", (*contacts)[*taille].email);
+
+    (*taille)++;
+    printf("[+] - contact ajouter !\n");
+}
+
+
+
 int main() {
+    Contact *contacts = NULL;
+    int taille = 0;
     int choix;
 
-    do{
+    do {
         printf("\n --------------< MENU >--------------\n");
         printf("1. Ajouter un contact \n");
         printf("2. Supprimer un contact \n");
         printf("3. Chercher un contact \n");
         printf("4. Afficher tous les contacts \n");
-        scanf("%d\n", &choix);
+        printf("5. quitter \n");
+        printf("Votre choix : ");
+        scanf("%d", &choix);
 
-        if (choix == 1) {
-            printf("[+] -> ajouter un contact\n");
-        } else if (choix == 2){
-            printf("[-] -> supprimer un contact\n");
-        } else if (choix == 3){
-            printf("[?] -> chercher un contact\n");
-        } else if (choix == 4){
-            printf("[A] -> tout les contact\n");
-        } else {
-            printf("[!] -> choix invalide, réessayez...\n");
+        switch (choix) {
+        case 1:
+            ajouter_contact(&contacts, &taille);
+            break;
+        case 2:
+            supprimer_contact();
+            break;
+        case 3:
+            chercher_contact();
+            break;
+        case 4:
+            afficher_contacts();
+            break;
+        case 5:
+            printf("au revoir le s !!!");
+            break;        
+        default:
+            printf("Choix invalide...");
         }
-    } while(choix != 5);
+        } while (choix != 5);
 
+    free(contacts);
     return 0;
 }
