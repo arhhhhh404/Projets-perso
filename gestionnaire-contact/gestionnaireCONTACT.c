fe1bbbafa1d5f6a98cbe2f47a0eb883637309a40
arhhhhh404 <mrtn.kerloch@email.com>
2025-05-20 00:50:57 +0200

    commit3

--- gestionnaire-contact/gestionnaireCONTACT.c
+++ gestionnaire-contact/gestionnaireCONTACT.c
@@ -13,7 +13,7 @@ void ajouter_contact(Contact **contacts, int *taille) {
     *contacts = realloc(*contacts, (*taille + 1)*sizeof(Contact));
 
     if (*contacts == NULL) {
-        printf("Erreur d'allocation mémoire\n");
+        printf("Erreur d'allocation mémoire \n");
         return;
     }
 
@@ -27,10 +27,43 @@ void ajouter_contact(Contact **contacts, int *taille) {
     scanf("%s", (*contacts)[*taille].email);
 
     (*taille)++;
-    printf("[+] - contact ajouter !\n");
+    printf("\n [+] - contact ajouter ! \n");
 }
 
+void chercher_contact(Contact *contacts, int taille) {
+    char nom_rechercher[50];
+    printf("Entrée le nom rechercher: ");
+    scanf("%s", nom_rechercher);
 
+    for(int i = 0; i < taille; i++) {
+        if (strcmp(nom_rechercher, contacts[i].nom)) {
+            printf("\n [?] - contact trouvé ! \n");
+            printf("Nom       : %s", contacts[i].nom);
+            printf("Prénom    : %s", contacts[i].prenom);
+            printf("Téléphone : %s", contacts[i].telephone);
+            printf("Email     : %s", contacts[i].email);
+        }
+        else{
+            printf("\n [?] - contact introuvable \n");
+        }
+    }
+
+}
+
+void afficher_contact(Contact *contacts, int taille) {
+    if (taille = 0) {
+        printf("\n aucun contact \n");
+        return;
+    }
+
+    for(int i = 0; i < taille; i++) {
+        printf("\n | Contact | \n");
+        printf("Nom       : %s", contacts[i].nom);
+        printf("Prénom    : %s", contacts[i].prenom);
+        printf("Téléphone : %s", contacts[i].telephone);
+        printf("Email     : %s", contacts[i].email);
+    }
+}
 
 int main() {
     Contact *contacts = NULL;
@@ -38,7 +71,7 @@ int main() {
     int choix;
 
     do {
-        printf("\n --------------< MENU >--------------\n");
+        printf("\n ----------------------------< MENU >---------------------------- \n");
         printf("1. Ajouter un contact \n");
         printf("2. Supprimer un contact \n");
         printf("3. Chercher un contact \n");
@@ -52,13 +85,13 @@ int main() {
             ajouter_contact(&contacts, &taille);
             break;
         case 2:
-            supprimer_contact();
+            supprimer_contact(&contacts, &taille);
             break;
         case 3:
-            chercher_contact();
+            chercher_contact(contacts, taille);
             break;
         case 4:
-            afficher_contacts();
+            afficher_contacts(contacts, taille);
             break;
         case 5:
             printf("au revoir le s !!!");
